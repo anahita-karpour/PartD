585a360b1eb845f53b0d16876c72f75bd6372c83
anahita-karpour <anahita.karpour@gmail.com>
2020-04-24 09:32:00 +1200

    Fourth change of PartD: my best day of the week is added.

--- PartD/src/com/anahitakarpour/PartDMain.java
+++ PartD/src/com/anahitakarpour/PartDMain.java
@@ -7,6 +7,7 @@ public class PartDMain {
 		System.out.println("Hi, my name is Anahita Karpour");
 		System.out.println("My favourite colour is very light cream");
 		System.out.println("My favourite mode of transportation is walking");
+		System.out.println("The best day of the week is Friday.");
 	}
 
 }
