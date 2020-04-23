8a8e42b1cc2a48bd606df026db071c99a4d832ad
anahita-karpour <anahita.karpour@gmail.com>
2020-04-24 09:19:34 +1200

    Second change of PartD: my favourite colour is added.

--- PartD/src/com/anahitakarpour/PartDMain.java
+++ PartD/src/com/anahitakarpour/PartDMain.java
@@ -5,7 +5,7 @@ public class PartDMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, my name is Anahita Karpour");
-		
+		System.out.println("My favourite colour is very light cream");
 	}
 
 }
